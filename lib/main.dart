a2acb4aea7a511a4538c8468a2aa36a150b34e96
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2022-09-19 20:39:03 +0200

    Added icon + sized it

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,14 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black12,
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('I AM RICH'),
           backgroundColor: Colors.blueGrey,
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDsHuCPImPnMprM6tH9rm_2KCsS4fLaIcYOgc_M4Q_l1WTQdiAWszeoJovZcwCRCmh5io&usqp=CAU',
-            ),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
